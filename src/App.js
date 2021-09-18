bb658984043b8ec425f77d779f00df8ae520ad55
Nikitos <power-horse@mail.ru>
2021-09-18 19:18:22 +0300

    Applyed css grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from './components/Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='container'>
+			<div className='my--header'>
+				<h1>To do list</h1>
+			</div>
+			<div className='cards'>
+				<Todo title='First task' />
+				<Todo title='Second task' />
+				<Todo title='Third task' />
+				<Todo title='Fourth task' />
+				<Todo title='Fifth task' />
+				<Todo title='Sixth task' />
+			</div>
+		</div>
+	);
 }
 
 export default App;
